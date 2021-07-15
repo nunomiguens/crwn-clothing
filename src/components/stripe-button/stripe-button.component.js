2a50063d1f44490596d2b22b614213aaac55ec02
nunomiguens <nunomiguens@gmail.com>
2021-07-15 16:12:43 +0100

    updating stripe integration and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51JDVJFHz65T8BG9cyslk4pHYn9zIpCCqjH3Y3DOsXSXRN4bh3HuGJD8wpu05zNoVDRtKQsCzrIPd2D49VxYb45YY00P0hxW53P';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is €${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
